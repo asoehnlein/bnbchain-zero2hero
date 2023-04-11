213cdc598e3f89a150dabf9fef3b84d51f89b003
Andreas Söhnlein (Tux) <andreas@soehnlein.dev>
2023-04-11 21:33:37 +0200

    Feat: optimise storage

--- security/homework-21-optimising-storage/contracts/Store.sol
+++ security/homework-21-optimising-storage/contracts/Store.sol
@@ -3,26 +3,26 @@ pragma solidity ^0.8.13;
 
 contract Store {
 
-    struct payments {
-        bool valid;
-        uint256 amount;
-        address sender;
-        uint8 paymentType;
-        uint256 finalAmount;
-        address receiver;
-        uint256 initialAmount;
-        bool checked;
-    }
-    uint8 index;
     uint256 public number;
-    bool flag1;
-    address admin;
     mapping (address=>uint256) balances;
+    bool flag1;
     bool flag2;
-    address admin2;
     bool flag3;
+    uint8 index;
+    address admin;
+    address admin2;
     payments[8] topPayments;
 
+    struct payments {
+        uint256 initialAmount;
+        uint256 finalAmount;
+        uint256 amount;
+        uint8 paymentType;
+        bool valid;
+        bool checked;
+        address sender;
+        address receiver;
+    }
 
     constructor(){
 
